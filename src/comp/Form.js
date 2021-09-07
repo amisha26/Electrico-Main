d22dabe5aa3864e08519744b27d3d23772bc2572
amisha26 <amisha8299@gmail.com>
2021-09-07 22:40:10 +0530

    site deployment done

--- src/comp/Form.js
+++ src/comp/Form.js
@@ -48,7 +48,7 @@ export default function Form() {
   };
 
   //******************** API Call *************************
-  const api = "https://elctro-api.herokuapp.com/api/v1/shop/";
+  const api = "";
 
   // user login request
   const formSubmit = ({ x }) => {
